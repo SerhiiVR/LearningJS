7a06460014b62e15ec0cb0bfb20901094bb845fa
Serhii <gorn3510@gmail.com>
2020-11-06 15:34:22 +0200

    Created index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("/", function (req, res) {
+
+   let a = document.getElementsByName('data');
+   console.log(a.length + "f");
+    res.sendFile(__dirname+'/index.html');
+
+
+    setTimeout(() => {
+       // res.send("hi");
+    }, 400);
+
+}
+);
+
+app.listen(3000);
